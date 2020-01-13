7a51d056e6db8e08b328b9d3a413e322fafed55e
super <supereggsong@gmail.com>
2020-01-13 14:22:00 +0900

    asus

--- app.js
+++ app.js
@@ -1,66 +1,77 @@
-const createError = require('http-errors');
-const compression = require('compression');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const logger = require('morgan');
-require('dotenv').config();
+const createError = require("http-errors");
+const compression = require("compression");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const logger = require("morgan");
+require("dotenv").config();
 
-const passport = require('passport');
-const passportConfig = require('./passport');
+const passport = require("passport");
+const passportConfig = require("./passport");
 const app = express();
 
-const indexRouter = require('./router/index');
-const adminRouter = require('./router/admin');
+const indexRouter = require("./router/index");
+const adminRouter = require("./router/admin");
 
 passportConfig(passport);
 
-const connect = require('./model');
+const connect = require("./model");
 connect();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.set('port', process.env.PORT || 8001);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.set("port", process.env.PORT || 8001);
 
 app.use(compression());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(express.static(path.join(__dirname, 'public'), {maxAge:'1d'}));
-app.use('/paper-kit', express.static(path.join(__dirname, 'public/paper-kit'), {maxAge: '1m'}));
-app.use('/cache_img', express.static(path.join(__dirname, 'cache_img'), {maxAge: '1m'}));
-app.use('/nm', express.static(path.join(__dirname, 'node_modules'), {maxAge: '1m'}));
-app.use(session({
-  secret: process.env.COOKIE_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {secure: false, httpOnly: true}
-}));
+app.use(express.static(path.join(__dirname, "public"), { maxAge: "" }));
+app.use(
+  "/paper-kit",
+  express.static(path.join(__dirname, "public/paper-kit"), { maxAge: "1m" })
+);
+app.use(
+  "/cache_img",
+  express.static(path.join(__dirname, "cache_img"), { maxAge: "1m" })
+);
+app.use(
+  "/nm",
+  express.static(path.join(__dirname, "node_modules"), { maxAge: "1m" })
+);
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, httpOnly: true }
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/admin', adminRouter);
+app.use("/", indexRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.get('port')}번 포트에서 대기 중`);
+app.listen(app.get("port"), () => {
+  console.log(`${app.get("port")}번 포트에서 대기 중`);
 });
--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -39,11 +39,18 @@ $(document).ready(function() {
   // kakao map
   let container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
   let options = { //지도를 생성할 때 필요한 기본 옵션
-    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+    center: new kakao.maps.LatLng(37.45515671610494,126.90146922122503), //지도의 중심좌표.
     level: 3 //지도의 레벨(확대, 축소 정도)
   };
   let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+  var markerPosition  = new kakao.maps.LatLng(37.45515671610494,126.90146922122503);
 
+// 마커를 생성합니다
+  var marker = new kakao.maps.Marker({
+    position: markerPosition
+  });
+  // 마커가 지도 위에 표시되도록 설정합니다
+  marker.setMap(map);
   // dropdown a href 클릭시 기본 막기
   $('nav a[data-toggle]').on('click', function(e){
     // e.stopPropagation();
